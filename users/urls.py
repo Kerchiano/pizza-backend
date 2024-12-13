96b40b1b3556c264fe62f4f781d76a2825887214
Kerchiano <beskirill127@gmail.com>
2024-12-13 17:46:24 +0200

    Fix

--- users/urls.py
+++ users/urls.py
@@ -1,8 +1,13 @@
 from django.urls import path, include
 from users.views import CustomTokenObtainPairView
+from djoser.views import UserViewSet
 
 urlpatterns = [
     path('auth/jwt/create/', CustomTokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('auth/', include('djoser.urls')),
+    path('auth/users/', UserViewSet.as_view({'post': 'create'}), name='user_create'),
+    path('auth/users/me/', UserViewSet.as_view({
+        'get': 'me',
+        'patch': 'me',
+    }), name='user_me'),
     path('auth/', include('djoser.urls.jwt')),
 ]
