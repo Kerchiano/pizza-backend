6725b6775b148b7b817d21f2c14bf53cff167ccd
Kerchiano <beskirill127@gmail.com>
2024-12-15 18:08:40 +0200

    Test deploy

--- pizza_backend/urls.py
+++ pizza_backend/urls.py
@@ -38,8 +38,8 @@ schema_view = get_schema_view(
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include(users_urls)),
-    path('', include(categories_urls)),
+    path('users', include(users_urls)),
+    path('shop', include(categories_urls)),
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
