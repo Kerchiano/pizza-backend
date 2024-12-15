ae42bf40820448afdd02b8e0a0e49095f21d4485
Kerchiano <beskirill127@gmail.com>
2024-12-15 18:26:07 +0200

    Test deploy

--- pizza_backend/urls.py
+++ pizza_backend/urls.py
@@ -25,6 +25,7 @@ from django.urls import path
 from drf_yasg import openapi
 from drf_yasg.views import get_schema_view
 from rest_framework import permissions
+from django.shortcuts import redirect
 
 schema_view = get_schema_view(
     openapi.Info(
@@ -42,4 +43,9 @@ urlpatterns = [
     path('shop', include(categories_urls)),
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+    path('', lambda request: redirect('schema-swagger-ui')),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
