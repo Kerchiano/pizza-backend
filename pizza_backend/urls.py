689a5f0f396b38709b9f273792ab8dba76a9c116
Kerchiano <beskirill127@gmail.com>
2024-12-16 14:30:11 +0200

    Test deploy

--- pizza_backend/urls.py
+++ pizza_backend/urls.py
@@ -25,13 +25,12 @@ from django.urls import path
 from drf_yasg import openapi
 from drf_yasg.views import get_schema_view
 from rest_framework import permissions
-from django.shortcuts import redirect
 
 schema_view = get_schema_view(
     openapi.Info(
-        title="Your API Title",
+        title="Pizza backend",
         default_version='v1',
-        description="API documentation",
+        description="Pizza API",
     ),
     public=True,
     permission_classes=([permissions.AllowAny]),
@@ -41,9 +40,8 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include(users_urls)),
     path('', include(categories_urls)),
-    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
-    path('', lambda request: redirect('schema-swagger-ui')),
 ]
 
 if settings.DEBUG:
